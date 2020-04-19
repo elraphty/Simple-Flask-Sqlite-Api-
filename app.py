ef9c327a18598edfde9e414c9ff19c6240c84e33
elraphty <elraphtyofficial@gmail.com>
2020-04-20 00:16:34 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Init App
+app = Flask(__name__)
+basedir = os.path.join(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# init db
+SQLAlchemy(app)
+
+# init marshmallow
+Marshmallow(app)
+
+@app.route('/', methods=['GET'])
+def index():
+  return jsonify({'msg': 'hello'})
+
+
+## Run
+if __name__ == "__main__":
+    app.run(debug=True)
