cee5cc9c57a8f3d7c1f84ed163177acccbcdb3ea
elraphty <elraphtyofficial@gmail.com>
2020-04-20 22:38:42 +0100

    created product schema

--- app.py
+++ app.py
@@ -12,15 +12,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # init db
-SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 # init marshmallow
-Marshmallow(app)
+ma = Marshmallow(app)
+
+# product class
+class Product(db.Model): 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), unique=True)
+  description = db.Column(db.String(200))
+  price = db.Column(db.Float)
+  qty = db.Column(db.Integer)
+
+  def __init__(self, name, description, price, qty):
+    self.name = name
+    self.description = description
+    self.price = price
+    self.qty = qty
+ 
 
 @app.route('/', methods=['GET'])
 def index():
   return jsonify({'msg': 'hello'})
 
+# product schema
+class ProductSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'description', 'price', 'qty')
+
+# init schema
+product_schema = ProductSchema(strict=True)
+products_schema = ProductSchema(many=True, strict=True)
 
 ## Run
 if __name__ == "__main__":
